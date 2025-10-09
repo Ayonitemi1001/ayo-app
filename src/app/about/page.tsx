c28e8188caca89a359590f18c6eef8944f6eeb0c
Ayonitemi1001 <ogunniranayonitemi3@gmail.com>
2025-10-09 10:18:10 +0100

    feat: add initial implementation of page and file components with logging functionality

---
+++ src/app/about/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function page() {
+    function function1() {
+    console.log('Hello');
+    console.log(2 + 2);
+}
+
+function1();
+  
+  
+  
+  
+  
+    return (
+    <div>page</div>
+  )
+}
